ddc61d6f874b1bf29bcf4ed03a3e58c9171e6d98
Allen-Sarmiento <sarmientoallenrey@gmail.com>
2018-10-15 15:03:35 -0700

    my revision

--- madlibs.cpp
+++ madlibs.cpp
@@ -5,7 +5,7 @@ int main()
 {
   int number;
   std::cout << "Welcome to madlibs! \n"
-  << "Choose a number between 1-4: ";
+            << "Choose a number between 1-4: ";
   std::cin >> number;
 
   if(number == 1) {
@@ -24,17 +24,37 @@ int main()
     std::getline(std::cin, noun);
 
     std::cout << "Please excuse " << name <<". \n"
-    << "who is far too " << adjective << std::endl
-    << "to attend " << noun << "class. \n";
+              << "who is far too " << adjective << std::endl
+              << "to attend " << noun << "class. \n";
 
   } else if(number == 2) {
     std::string name, noun, type_of_fluid, substance;
 
-    std::cin.ignore;
+    std::cin.ignore();
     
+  } else if(number == 4) {
+    std::string name, 
+                noun, 
+                event;
+
+    std::cin.ignore();
+
+    std::cout << "Enter a name: ";
+    std::getline(std::cin, name);
+
+    std::cout << "Enter a noun: ";
+    std::getline(std::cin, noun);
+
+    std::cout << "Enter an event: ";
+    std::getline(std::cin, event);
+
+    std::cout  << name << " is too cool\n"
+               << "for " << noun << " class.\n"
+               << "instead, she/he will be\n"
+               << "attending the " << event << ".\n";
+  } else {
+    std::cout << "YOU ENTERED THE WRONG VALUE! I SAID 1-4!!   ＼(｀0´)／" << std::endl;
   }
 
-
-
-
+  return 0;
 }
